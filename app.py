04b96b8a98ac8eb48993b63164ecf9ec8966357b
Victor Pires <vctprs@gmail.com>
2023-04-14 13:27:53 -0300

    .

--- app.py
+++ app.py
@@ -4,14 +4,14 @@ import sqlite3 as sql
 app = Flask(__name__)
 app.secret_key = 'mysecretkey'
 
-# rota para exibir a página inicial
+# Rota para exibir a página inicial
 @app.route("/")
 @app.route("/books")
 def home():
     books = fetch_all_books()
     return render_template('book/books.html', books=books)
 
-# rota para exibir um livro específico
+# Rota para exibir um livro específico
 @app.route("/books/<int:id>")
 def get_book(id):
     con = sql.connect('library.db')
@@ -25,12 +25,12 @@ def get_book(id):
     book_info = map_book(book)
     return render_template('book/book.html', book=book_info)
 
-# rota para exibir o formulário de cadastro de livro
+# Rota para exibir o formulário de cadastro de livro
 @app.route("/book_form")
 def book_form():
     return render_template('book/book_form.html')
 
-# rota para cadastrar um novo livro
+# Rota para cadastrar um novo livro
 @app.route("/cadastro_livro", methods=["POST"])
 def cadastrar_livro():
     try:
@@ -54,7 +54,7 @@ def cadastrar_livro():
         flash("Erro ao cadastrar livro: " + str(e))
         return redirect(url_for('book_form'))
 
-# função para buscar todos os livros cadastrados no banco de dados
+# Função para buscar todos os livros cadastrados no banco de dados
 def fetch_all_books():
     con = sql.connect('library.db')
     cur = con.cursor()
@@ -68,13 +68,13 @@ def fetch_all_books():
     books_resp = list(map(map_book, books))
     return books_resp
 
-# função para mapear os dados do banco de dados para um dicionário
+# Função para mapear os dados do banco de dados para um dicionário
 def map_book(book):
     return {
         "id": book[0],
         "name": book[1],
         "author": book[2],
-        "is_available": "Yes" if book[3] == 1 else "No"
+        "is_available": "Sim" if book[3] == 1 else "Não"
     }
 
 
@@ -91,7 +91,7 @@ def edit_book_form(id):
     book_info = map_book(book)
     return render_template('book/edit_book.html', book=book_info)
 
-# rota para atualizar um livro específico
+# Rota para atualizar um livro específico
 
 @app.route("/books/editar/<int:id>", methods=["POST"])
 def edit_book(id):
@@ -115,6 +115,7 @@ def edit_book(id):
         flash("Erro ao atualizar livro: " + str(e))
         return redirect(url_for('edit_book_form', id=id))
 
+# Rota para excluir um livro específico
 
 @app.route('/delete_book/<int:id>', methods=['GET', 'POST'])
 def delete_book(id):
